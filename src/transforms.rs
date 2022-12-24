92c991bd160dadec65b9174c996261c3bfa31cda
Jesse Conner <jconner90@gmail.com>
2022-12-24 17:16:27 -0500

    All rotations mapped

--- src/transforms.rs
+++ src/transforms.rs
@@ -40,42 +40,112 @@ fn fp(mut working_cube: Cube) -> Cube {
 }
 
 fn r(mut working_cube: Cube) -> Cube {
+    rotate_clockwise(
+        &mut working_cube.blue,
+        &mut working_cube.orange,
+        &mut working_cube.yellow,
+        &mut working_cube.red,
+        &mut working_cube.white,
+    );
     working_cube
 }
 
 fn rp(mut working_cube: Cube) -> Cube {
+    rotate_counter_clockwise(
+        &mut working_cube.blue,
+        &mut working_cube.orange,
+        &mut working_cube.yellow,
+        &mut working_cube.red,
+        &mut working_cube.white,
+    );
     working_cube
 }
 
 fn l(mut working_cube: Cube) -> Cube {
+    rotate_clockwise(
+        &mut working_cube.green,
+        &mut working_cube.orange,
+        &mut working_cube.white,
+        &mut working_cube.red,
+        &mut working_cube.yellow,
+    );
     working_cube
 }
 
 fn lp(mut working_cube: Cube) -> Cube {
+    rotate_counter_clockwise(
+        &mut working_cube.green,
+        &mut working_cube.orange,
+        &mut working_cube.white,
+        &mut working_cube.red,
+        &mut working_cube.yellow,
+    );
     working_cube
 }
 
 fn u(mut working_cube: Cube) -> Cube {
+    rotate_clockwise(
+        &mut working_cube.orange,
+        &mut working_cube.yellow,
+        &mut working_cube.blue,
+        &mut working_cube.white,
+        &mut working_cube.green,
+    );
     working_cube
 }
 
 fn up(mut working_cube: Cube) -> Cube {
+    rotate_counter_clockwise(
+        &mut working_cube.orange,
+        &mut working_cube.yellow,
+        &mut working_cube.blue,
+        &mut working_cube.white,
+        &mut working_cube.green,
+    );
     working_cube
 }
 
 fn d(mut working_cube: Cube) -> Cube {
+    rotate_clockwise(
+        &mut working_cube.red,
+        &mut working_cube.orange,
+        &mut working_cube.blue,
+        &mut working_cube.yellow,
+        &mut working_cube.green,
+    );
     working_cube
 }
 
 fn dp(mut working_cube: Cube) -> Cube {
+    rotate_counter_clockwise(
+        &mut working_cube.red,
+        &mut working_cube.orange,
+        &mut working_cube.blue,
+        &mut working_cube.yellow,
+        &mut working_cube.green,
+    );
     working_cube
 }
 
 fn b(mut working_cube: Cube) -> Cube {
+    rotate_clockwise(
+        &mut working_cube.yellow,
+        &mut working_cube.red,
+        &mut working_cube.green,
+        &mut working_cube.orange,
+        &mut working_cube.blue,
+    );
     working_cube
 }
 
 fn bp(mut working_cube: Cube) -> Cube {
+    rotate_counter_clockwise(
+        &mut working_cube.yellow,
+        &mut working_cube.red,
+        &mut working_cube.green,
+        &mut working_cube.orange,
+        &mut working_cube.blue,
+    );
     working_cube
 }
 
