1eb65c558830d58c2ef276ba18e1e159cdb65696
Jesse Conner <jconner90@gmail.com>
2022-12-26 07:41:53 -0500

    back rotations working

--- src/transforms.rs
+++ src/transforms.rs
@@ -228,26 +228,44 @@ fn dp(mut working_cube: Cube) -> Cube {
 }
 
 fn b(mut working_cube: Cube) -> Cube {
-    // rotate_clockwise(
-    //     &mut working_cube.yellow,
-    //     &mut working_cube.red,
-    //     &mut working_cube.green,
-    //     &mut working_cube.orange,
-    //     &mut working_cube.blue,
-    //     Orientation::Z,
-    // );
+    working_cube.yellow = rotate_face_counter_clockwise(working_cube.yellow);
+    let temp_top_edge = working_cube.orange.clone();
+    let temp_right_edge = working_cube.green.clone();
+    let temp_down_edge = working_cube.red.clone();
+    let temp_left_edge = working_cube.blue.clone();
+    working_cube.orange.t.l = temp_right_edge.b.l;
+    working_cube.orange.t.c = temp_right_edge.m.l;
+    working_cube.orange.t.r = temp_right_edge.t.l;
+    working_cube.green.t.l = temp_down_edge.b.l;
+    working_cube.green.m.l = temp_down_edge.b.c;
+    working_cube.green.b.l = temp_down_edge.b.r;
+    working_cube.red.b.l = temp_left_edge.b.r;
+    working_cube.red.b.c = temp_left_edge.m.r;
+    working_cube.red.b.r = temp_left_edge.t.r;
+    working_cube.blue.t.r = temp_top_edge.t.l;
+    working_cube.blue.m.r = temp_top_edge.t.c;
+    working_cube.blue.b.r = temp_top_edge.t.r;
     working_cube
 }
 
 fn bp(mut working_cube: Cube) -> Cube {
-    // rotate_counter_clockwise(
-    //     &mut working_cube.yellow,
-    //     &mut working_cube.red,
-    //     &mut working_cube.green,
-    //     &mut working_cube.orange,
-    //     &mut working_cube.blue,
-    //     Orientation::Z,
-    // );
+    working_cube.yellow = rotate_face_clockwise(working_cube.yellow);
+    let temp_orange_face = working_cube.orange.clone();
+    let temp_green_face = working_cube.green.clone();
+    let temp_red_face = working_cube.red.clone();
+    let temp_blue_face = working_cube.blue.clone();
+    working_cube.orange.t.l = temp_blue_face.t.r;
+    working_cube.orange.t.c = temp_blue_face.m.r;
+    working_cube.orange.t.r = temp_blue_face.b.r;
+    working_cube.green.t.l = temp_orange_face.t.r;
+    working_cube.green.m.l = temp_orange_face.t.c;
+    working_cube.green.b.l = temp_orange_face.t.l;
+    working_cube.red.b.l = temp_green_face.t.l;
+    working_cube.red.b.c = temp_green_face.m.l;
+    working_cube.red.b.r = temp_green_face.b.l;
+    working_cube.blue.t.r = temp_red_face.b.r;
+    working_cube.blue.m.r = temp_red_face.b.c;
+    working_cube.blue.b.r = temp_red_face.b.l;
     working_cube
 }
 
